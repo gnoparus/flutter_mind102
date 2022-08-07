9557155b6728055b0658daba0df0ff7c77db073d
gnoparus <surapong@gmail.com>
2022-08-07 13:52:11 +0700

    Init import

---
+++ android/app/src/main/kotlin/com/example/flutter_mind102/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_mind102
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      print("Answer chosen!");
+      _questionIndex += 1;
+      print(_questionIndex);
+    });
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("This is AppBar Title"),
+        ),
+        body: Column(
+          children: [
+            Text(questions[_questionIndex]),
+            ElevatedButton(onPressed: (() {}), child: const Text("Answer 1")),
+            ElevatedButton(onPressed: (() {}), child: const Text("Answer 2")),
+            ElevatedButton(
+                onPressed: (() => _answerQuestion()),
+                child: const Text("Answer 3"))
+          ],
+        ),
+      ),
+    );
+  }
+}
