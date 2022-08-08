7528067c745d7d64d99c4c69e90d8fa7f1a396cd
gnoparus <surapong@gmail.com>
2022-08-08 19:10:48 +0700

    Make grade from score step

--- lib/result.dart
+++ lib/result.dart
@@ -4,10 +4,28 @@ class Result extends StatelessWidget {
   final int totalScore;
   const Result(this.totalScore, {Key? key}) : super(key: key);
 
+  String get resultPhrase {
+    var resultText = "You are normal.";
+
+    if (totalScore < 200) {
+      resultText = "Pretty likable.";
+    } else if (totalScore < 300) {
+      resultText = "You are monster.";
+    } else if (totalScore < 400) {
+      resultText = "You are so bad.";
+    } else if (totalScore < 500) {
+      resultText = "You are angel.";
+    } else {
+      resultText = "You are hero.";
+    }
+
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it! $totalScore'),
+      child: Text(resultPhrase),
     );
   }
 }
